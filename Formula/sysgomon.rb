20a33bf28c2052ee9268af5e088a2525245a77b0
Samir Patel <samir@getreallabs.com>
2025-03-29 15:14:35 -0500

    Update Homebrew formula to version 0.1.1

--- Formula/sysgomon.rb
+++ Formula/sysgomon.rb
@@ -1,8 +1,8 @@
 class Sysgomon < Formula
   desc "A beautiful and efficient terminal-based system monitoring tool"
   homepage "https://github.com/samirspatel/sysgomon"
-  url "https://github.com/samirspatel/sysgomon/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "75d77d161370496f8321560c2f89121c4f591a07119b13e141008a51a9c78666"
+  url "https://github.com/samirspatel/sysgomon/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "e7a5412de43c39790c916e9ba142f552482635e57d9832bc6288698b7b04b447"
 
   depends_on "go" => :build
 
@@ -11,6 +11,6 @@ class Sysgomon < Formula
   end
 
   test do
-    assert_match "SysGoMon version 0.1.0", shell_output("#{bin}/sysgomon --version")
+    assert_match "SysGoMon version 0.1.1", shell_output("#{bin}/sysgomon --version")
   end
 end 
